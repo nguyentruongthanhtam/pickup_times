c24d2ed934776231d4843b1e3709f516ed0bb28d
unknown <nguyentruongthanhtam@users.noreply.github.com>
2019-01-22 14:42:09 +0200

    Updates

---
+++ precache-manifest.bf2dac0a9d0c04e6c4f02597b987c96b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f8db2e2a194b615772fad97ebab6990",
+    "url": "/pickup_times/static/media/pickup_times.3f8db2e2.csv"
+  },
+  {
+    "revision": "f4d9778313040491f51af91b2896bb38",
+    "url": "/pickup_times/static/media/locations.f4d97783.csv"
+  },
+  {
+    "revision": "63b8db519b265560ca33",
+    "url": "/pickup_times/static/js/runtime~main.63b8db51.js"
+  },
+  {
+    "revision": "381c4425b9c9e09507ac",
+    "url": "/pickup_times/static/js/main.381c4425.chunk.js"
+  },
+  {
+    "revision": "ae75e61786f1861ce133",
+    "url": "/pickup_times/static/js/1.ae75e617.chunk.js"
+  },
+  {
+    "revision": "381c4425b9c9e09507ac",
+    "url": "/pickup_times/static/css/main.4f6d4e9d.chunk.css"
+  },
+  {
+    "revision": "ae75e61786f1861ce133",
+    "url": "/pickup_times/static/css/1.d6948535.chunk.css"
+  },
+  {
+    "revision": "3212bedd023e2765e5a8f6a78fffef9c",
+    "url": "/pickup_times/index.html"
+  }
+];
